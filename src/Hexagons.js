c2f35f1246704416f4805e0566fb91d83ec33b29
Jaiden Mispy <jaiden@mispy.me>
2017-02-27 03:27:54 +1100

    svg works, but let's try canvas

--- src/Hexagons.js
+++ src/Hexagons.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import * as d3 from 'd3'
 import 'd3-random'
-import {interpolateOranges} from 'd3-scale-chromatic'
+import * as d3_chromatic from 'd3-scale-chromatic'
 import {hexbin as d3_hexbin} from 'd3-hexbin'
 import Resizable from 'react-component-resizable'
 import React, {Component} from 'react'
@@ -10,8 +10,18 @@ import {computed, observable, action} from 'mobx'
 import {bind} from 'decko'
 import styles from './index.css'
 
-function dist(a, b) {
-    return (b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y)
+function getDistance(a, b) {
+    return Math.sqrt((b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y))
+}
+
+class Ripple {
+    @observable radius
+
+    constructor({origin, colorScale, radius}) {
+        this.origin = origin
+        this.colorScale = colorScale
+        this.radius = radius
+    }
 }
 
 @observer
@@ -23,60 +33,109 @@ class HexagonsMain extends Component {
         return this.props.height
     }
 
-    @computed get hexRadius() {
-        return 6
+    @computed get spacing() {
+        return 10        
     }
 
-    @computed get hexbin() {
-        return d3_hexbin().radius(this.hexRadius)
+    @computed get theta() {
+        return Math.PI * (3 - Math.sqrt(5))
     }
 
-    @computed get points() {
+    constructor() {
+        super()
+        this.points = d3.range(1000).map(i => {return { x: 0, y: 0 }})
+    }
+
+    updatePoints() {
         const {width, height} = this.props
-        if (!width || !height) return []
+        if (!width || !height) return
 
-        const {rotation} = this
+        const {points, rotation, spacing, theta} = this
 
-        const radius = 5
-        var theta = Math.PI * (3 - Math.sqrt(5));
-        return d3.range(1000).map(i => {
-            var r = radius * Math.sqrt(i), a = theta * i * rotation;
-            return {
-              x: width / 2 + r * Math.cos(a),
-              y: height / 2 + r * Math.sin(a)
-            };
-        })
+        for (let i = 0; i < points.length; i++) {
+            const radius = spacing * Math.sqrt(i)
+            const angle = theta * i * rotation
+            let x = width / 2 + radius * Math.cos(angle)
+            let y = height / 2 + radius * Math.sin(angle)
+
+            points[i].x = x
+            points[i].y = y
+        }
     }
 
-    @computed get hexagons() {
-        const {hexbin, points} = this
-        return hexbin(points.map(point => [point.x, point.y]))
-    }
-
-    @observable rotation = 1
+    colorScales = _(d3_chromatic).keys().filter(k => k.indexOf('interpolate') !== -1).map(k => d3_chromatic[k]).value()
+    colorScalesIndex = 0
+    @observable rotation = +(new Date())/1000000000//0.1952444
+    @observable isPlaying = true
+    @observable bbox = null
+    @observable mouse = { x: 0, y: 0 }
+    @observable ripples = []
 
     componentDidMount() {
-        d3.timer(() => {
-            this.rotation += 0.00002
-        })
+        this.bbox = this.base.getBBox()
+        this.updatePoints()
+        this.circles = d3.select(this.base).selectAll('circle')
+        d3.timer(this.frame)
+    }
+
+    @action.bound onMouseMove(e) {
+        const newMouse = { x: e.offsetX, y: e.offsetY }
+        this.mouse = newMouse
+    }
+
+    @action.bound onClick(e) {
+        this.colorScalesIndex = this.colorScalesIndex >= this.colorScales.length-1 ? 0 : this.colorScalesIndex+1     
+
+        const {colorScales, colorScalesIndex, mouse, points} = this
+
+        const closestPointToMouse = _.orderBy(points, point => getDistance(point, mouse))[0]
+
+        if (this.ripples.length >= 50)
+            this.ripples = this.ripples.slice(1)
+
+        this.ripples.push(new Ripple({
+            origin: closestPointToMouse,
+            colorScale: d3.scaleSequential(colorScales[colorScalesIndex]).domain([0, this.width/2]),
+            radius: 0
+        }))
+
+
+        e.preventDefault()
+    }
+
+    @action.bound frame() {
+        this.rotation += 0.000001//0.000001///Math.pow(dist(this.mouse, { x: this.width/2, y: this.height/2 }), 2)
+
+        this.updatePoints()
+
+        const {points, ripples} = this
+
+        for (let ripple of ripples) {
+            if (ripple.radius < this.width*2)
+                ripple.radius += 10
+        }
+
+        this.circles
+            .data(points)
+            .attr('transform', d => `translate(${d.x}, ${d.y})`)
+            .attr('fill', d => {
+                for (let ripple of ripples.reverse()) {
+                    const dist = getDistance(d, ripple.origin)
+                    if (dist < ripple.radius)
+                        return ripple.colorScale(dist)
+                }
+            })
     }
 
     render() {
-        let {width, height, hexbin, hexagons, rotation, points} = this
+        console.log("render")
+        let {width, height, points, bbox} = this
 
-        const maxSize = 2
-        const colorScale = d3.scaleSequential(interpolateOranges).domain([-20000, Math.pow(width/2, 2)])
-        const center = { x: width/2, y: height/2 }
-
-        return <g style={{stroke: '#f5aa44', fill: '#f5aa44', 'stroke-width': '1px'}}>
-            {_.map(points, (d, i) => 
-                <circle cx={d.x} cy={d.y} r={3}/>
+        return <g onClick={this.onClick} onMouseMove={this.onMouseMove} style={{fill: '#f5aa44', 'stroke-width': '1px', cursor: 'pointer', 'opacity': 0.5}}>
+            {bbox && <rect x={0} y={0} width={width} height={height} fill="rgba(0,0,0,0)" stroke="none"/>}
+            {_.map(points, (d,i) => 
+                <circle cx={0} cy={0} r={5}/>
             )}
-            {/*_.map(hexagons, (d, i) => {
-                const distFromCenter = dist(d, center)
-                return <path onMouseOver={this.onMouseOver} d={`M${d.x},${d.y}${hexbin.hexagon()}`} style={{fill: colorScale(distFromCenter), opacity: d.length/maxSize}}/>
-            }
-            )*/}
         </g>
     }
 }
