b8c7fd2db1aa7d8effde0409a2d16297219d6355
Jaiden Mispy <jaiden@mispy.me>
2017-12-16 00:25:40 +0800

    Fix missing comma

--- src/Goals.tsx
+++ src/Goals.tsx
@@ -53,7 +53,7 @@ const ZeroHunger: Goal = {
     slug: "zero-hunger",
     name: "Zero Hunger",
     title: "End hunger, achieve food security and improved nutrition and promote sustainable agriculture",
-    description: `A profound change of the global food and agriculture system is needed if we are to nourish today’s 795 million hungry and the additional 2 billion people expected by 2050.`
+    description: `A profound change of the global food and agriculture system is needed if we are to nourish today’s 795 million hungry and the additional 2 billion people expected by 2050.`,
     targets: [
         {
             // 2.1
