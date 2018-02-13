97a63a5ef10779bfad7c6f0c832d74617b9c895f
Jaiden Mispy <jaiden@mispy.me>
2018-02-14 04:17:48 +0800

    Fix header links

--- src/SiteHeader.tsx
+++ src/SiteHeader.tsx
@@ -3,11 +3,11 @@ import * as React from 'react'
 export const SiteHeader = () => {
     return <header>
         <div className="container">
-            <a className="logo" href="/">Our World in Data</a>
+            <a className="logo" href="https://ourworldindata.org">Our World in Data</a>
             <nav>
-                <a href="/blog">Blog</a>
-                <a href="/about">About</a>
-                <a href="/support">Donate</a>
+                <a href="https://ourworldindata.org/blog">Blog</a>
+                <a href="https://ourworldindata.org/about">About</a>
+                <a href="https://ourworldindata.org/support">Donate</a>
             </nav>
         </div>
     </header>
