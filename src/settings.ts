344051c2428bd14735a0b430f8226c88c8fe52c5
Jaiden Mispy <jaiden@mispy.me>
2018-02-28 16:06:05 +0800

    BAKED_PATH

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 import * as urljoin from 'url-join'
 
 export const BASE_PATH = "/sdgs"
-export const BAKED_URL = "http://l:8080" + BASE_PATH
+export const BAKED_URL = "https://ourworldindata.org" + BASE_PATH
 
 export function sdgsUrl(path: string) {
     return urljoin(BASE_PATH, path)
