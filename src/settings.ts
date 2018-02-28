b41c89bffc996f18fa76f3a09b487a7c5e0f5caa
Jaiden Mispy <jaiden@mispy.me>
2018-02-28 20:28:04 +0800

    Tweak urls

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 import * as urljoin from 'url-join'
 
-export const BASE_PATH = ""
-export const BAKED_URL = "https://sdg-tracker.org" + BASE_PATH
+export const BASE_PATH = "/"
+export const BAKED_URL = urljoin("https://sdg-tracker.org", BASE_PATH)
 
 export function sdgsUrl(path: string) {
     return urljoin(BASE_PATH, path)
