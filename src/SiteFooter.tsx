196f6b209b8067ced507b4f11b42689c9440fa07
Jaiden Mispy <jaiden@mispy.me>
2018-02-28 20:12:49 +0800

    Need diff embed url

--- src/SiteFooter.tsx
+++ src/SiteFooter.tsx
@@ -4,6 +4,6 @@ import {absoluteSdgsUrl} from './settings'
 export default function SiteFooter() {
     return <footer className="SiteFooter">
         <a href="https://goo.gl/forms/fwJmzRk68IVoPssh2">Give feedback on this project</a>
-        <script src={absoluteSdgsUrl("../grapher/embedCharts.js")}/>
+        <script src={"https://ourworldindata.org/grapher/embedCharts.js"}/>
     </footer>
 }
