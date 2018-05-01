506867ba9bdbc755cb86bd975bac7c238614f220
Jaiden Mispy <jaiden@mispy.me>
2018-05-01 12:23:25 +0800

    Update to webpack 4

--- js/sdgs.tsx
+++ js/sdgs.tsx
@@ -3,7 +3,6 @@ require('../css/sdgs.scss')
 
 import * as React from 'react'
 import * as ReactDOMServer from 'react-dom/server'
-import * as Markdown from 'react-markdown'
 import * as path from 'path'
 
 import IndexPage from '../src/IndexPage'
@@ -13,8 +12,8 @@ import pages from '../pages'
 
 export default (locals: any, callback: (val: null, resp: any) => void) => {
     const output: {[key: string]: string} = {}
-    output['/index.html'] = ReactDOMServer.renderToStaticMarkup(<IndexPage goals={pages}/>)
-    for (const page of pages) {
+    output['/index.html'] = ReactDOMServer.renderToStaticMarkup(<IndexPage goals={pages as any[]}/>)
+    for (const page of pages as any[]) {
         const el = page.layout === "goal" ? <GoalPage {...page}/> : <AboutPage {...page}/>
         output[`/${page.slug}.html`] = ReactDOMServer.renderToStaticMarkup(el)
     }
--- webpack.config.js
+++ webpack.config.js
@@ -7,95 +7,96 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const Dotenv = require('dotenv-webpack');
 const fs = require('fs')
 
-const isProduction = process.argv.indexOf('-p') !== -1;
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production'
+    return {
+        context: __dirname,    
+        entry: {
+            sdgs: path.join(__dirname, 'js/sdgs.tsx'),
+            admin: path.join(__dirname, 'src/admin.entry.tsx')
+        },
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: 'assets/[name].js',
+            libraryTarget: 'umd'
+        },
+        node: {
+            fs: 'empty'
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx", ".css"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "awesome-typescript-loader"
+                },
+                {
+                    test: /\.png$/,
+                    loader: 'url-loader?limit=10000&publicPath=assets/&outputPath=assets/'
+                },
+                { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff&outputPath=assets/" },
+                { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?limit=10000&outputPath=assets/" },
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader?modules&importLoaders=1&localIdentName=[local]'],
+                    }),
+                },
+                {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'sass-loader'] })
+                },
+                {
+                    test: /\.md$/,
+                    loader: ['json-loader', 'front-matter-loader'],
+                },
+            ]
+        },
+        devServer: {
+            host: '0.0.0.0',
+            port: 3333,
+            inline: false
+        },
+        devtool: (isProduction ? false : "cheap-module-eval-source-map"),
+        plugins: [
+            new Dotenv(),
 
-module.exports = {
-    context: __dirname,    
-    entry: {
-        sdgs: path.join(__dirname, 'js/sdgs.tsx'),
-        admin: path.join(__dirname, 'src/admin.entry.tsx')
-    },
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'assets/[name].js',
-        libraryTarget: 'umd'
-    },
-    node: {
-        fs: 'empty'
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".css"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "awesome-typescript-loader"
-            },
-            {
-                test: /\.png$/,
-                loader: 'url-loader?limit=10000&publicPath=assets/&outputPath=assets/'
-            },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff&outputPath=assets/" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?limit=10000&outputPath=assets/" },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader?modules&importLoaders=1&localIdentName=[local]'],
-                }),
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'sass-loader'] })
-            },
-            {
-                test: /\.md$/,
-                loader: ['json-loader', 'front-matter-loader'],
-            },
-        ]
-    },
-    devServer: {
-        host: '0.0.0.0',
-        port: 3333,
-        inline: false
-    },
-    devtool: (isProduction ? false : "cheap-module-eval-source-map"),
-    plugins: [
-        new Dotenv(),
+            new ExtractTextPlugin('assets/[name].css'),
 
-        new ExtractTextPlugin('assets/[name].css'),
+            new StaticSiteGeneratorPlugin({
+                entry: 'sdgs',
+                locals: { 'isProduction': isProduction },
+                globals: { window: {} }
+            }),
 
-        new StaticSiteGeneratorPlugin({
-            entry: 'sdgs',
-            locals: { 'isProduction': isProduction },
-            globals: { window: {} }
-        }),
+            new CopyWebpackPlugin([
+                { from: 'img', to: 'img' },
+                { from: 'admin', to: 'admin' },
+                { from: 'public' }
+            ])
+        ].concat(isProduction ? [
+            /*new OptimizeCssAssetsPlugin({
+                assetNameRegExp: /\.bundle.*\.css$/,
+                cssProcessorOptions: { discardComments: { removeAll: true } }
+            }),*/
 
-        new CopyWebpackPlugin([
-            { from: 'img', to: 'img' },
-            { from: 'admin', to: 'admin' },
-            { from: 'public' }
-        ])
-    ].concat(isProduction ? [
-        /*new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.bundle.*\.css$/,
-            cssProcessorOptions: { discardComments: { removeAll: true } }
-        }),*/
-
-        /*new webpack.optimize.UglifyJsPlugin({
-            compress: {
-              warnings: false,
-              screw_ie8: true,
-              conditionals: true,
-              unused: true,
-              comparisons: true,
-              sequences: true,
-              dead_code: true,
-              evaluate: true,
-              if_return: true,
-              join_vars: true
-            },
-        })*/
-    ] : [])
+            /*new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                if_return: true,
+                join_vars: true
+                },
+            })*/
+        ] : [])
+    }
 }
