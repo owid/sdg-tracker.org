8ee9849c835d84c0b9e7ff39678c7cb6a522e556
Jaiden Mispy <jaiden@mispy.me>
2018-06-28 00:13:45 +0800

    Add citation to footer

--- src/SiteFooter.tsx
+++ src/SiteFooter.tsx
@@ -3,7 +3,8 @@ import {absoluteSdgsUrl, GRAPHER_ROOT} from './settings'
 
 export default function SiteFooter() {
     return <footer className="SiteFooter bg-dark">
-        <a href="https://goo.gl/forms/fwJmzRk68IVoPssh2">Give us feedback on the SDG Tracker</a>
+        <p><a href="https://goo.gl/forms/fwJmzRk68IVoPssh2">Give us feedback on the SDG Tracker</a></p>
+        <p>Citation: Ritchie, Roser, Mispy, Ortiz-Ospina. "Measuring progress towards the Sustainable Development Goals." <em>SDG-Tracker.org, website</em> (2018).</p>
         <script src={`${GRAPHER_ROOT}/embedCharts.js`}/>
     </footer>
 }
